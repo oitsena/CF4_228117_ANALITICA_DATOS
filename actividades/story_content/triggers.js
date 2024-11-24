06e68e03ba6486417b8cded4ee06839e17bd79b6
nataliabecerraoit <natalia.becerra@bitinstitute.co>
2024-11-24 16:41:13 -0500

    carga inicial

---
+++ actividades/story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6MWPyaZ2m9I":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
